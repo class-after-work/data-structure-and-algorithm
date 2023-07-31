ac29853ff7aa9d1ed091efd34c1d1674e9f1dfb3
IL KYO OH <81012135+qdaszx@users.noreply.github.com>
2023-08-01 01:32:44 +0900

    add linked list class

--- linked-list.mjs
+++ linked-list.mjs
@@ -4,3 +4,95 @@ export class Node {
     this.next = next
   }
 }
+
+export class LinkedList {
+  constructor() {
+    this.head = null;
+    this.count = 0;
+  }
+
+  printAll() {
+    let currentNode = this.head;
+    let texts = []
+
+    while (currentNode != null) {
+      texts.push(currentNode.data)
+      currentNode = currentNode.next;
+    }
+
+    console.log(`[${texts.join(", ")}]`)
+  }
+
+  clear() {
+    this.head = null;
+    this.count = 0;
+  }
+
+  insertAt(index, data) {
+    if (index > this.count || index < 0) {
+      throw new Error("범위를 넘어갔습니다.")
+    }
+
+    let newNode = new Node(data);
+
+    if (index === 0) {
+      newNode.next = this.head;
+      this.head = newNode;
+    } else {
+      let currentNode = this.head;
+
+      for (let i = 0; i < index - 1; i++) {
+        currentNode = currentNode.next;
+      }
+      newNode.next = currentNode.next;
+      currentNode.next = newNode;
+    }
+    this.count += 1;
+  }
+
+  insertLast(data) {
+    this.insertAt(this.count, data)
+  }
+
+  deleteAt(index) {
+    if (index >= this.count || index < 0) {
+      throw new Error("제거할 수 없습니다.")
+    }
+
+    let currentNode = this.head;
+
+    if (index === 0) {
+      let deletedNode = this.head;
+      this.head = this.head.next;
+      this.count -= 1;
+      return deletedNode
+    } else {
+      for (let i = 0; i < index - 1; i++) {
+        currentNode = currentNode.next;
+      }
+
+      let deletedNode = currentNode.next;
+      currentNode.next = currentNode.next.next;
+      this.count -= 1;
+      return deletedNode;
+    }
+  }
+
+  deleteLast() {
+    return this.deleteAt(this.count - 1)
+  }
+
+  getNodeAt(index) {
+    if (index >= this.count || index < 0) {
+      throw new Error("범위를 넘어갔습니다.")
+    }
+
+    let currentNode = this.head;
+
+    for (let i = 0; i < index; i++) {
+      currentNode = currentNode.next;
+    }
+
+    return currentNode
+  }
+}
--- test.mjs
+++ test.mjs
@@ -1,4 +1,4 @@
-import { Node } from "./linked-list.mjs";
+import { Node, LinkedList } from "./linked-list.mjs";
 
 let node1 = new Node(1);
 let node2 = new Node(2);
@@ -10,3 +10,39 @@ node2.next = node3;
 console.log(node1.data)
 console.log(node1.next.data)
 console.log(node1.next.next.data)
+
+let list = new LinkedList();
+
+console.log("insertAt()")
+list.insertAt(0, 0)
+list.insertAt(1, 1)
+list.insertAt(2, 2)
+list.insertAt(3, 3)
+list.insertAt(4, 4)
+list.printAll()
+
+console.log("clear()")
+list.clear()
+list.printAll()
+
+console.log("insetLast()")
+list.insertLast(0)
+list.insertLast(1)
+list.insertLast(2)
+list.printAll();
+
+console.log("deleteAt()")
+list.deleteAt(0);
+list.printAll();
+list.deleteAt(1);
+list.printAll();
+
+console.log("deleteLast()")
+list.deleteLast();
+list.printAll();
+
+console.log("getNodeAt()")
+list.insertLast(5);
+list.insertLast(4);
+list.insertLast(3);
+console.log(list.getNodeAt(1));
